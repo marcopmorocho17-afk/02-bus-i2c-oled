a04f7385fd8e3667dc253b1781376d521a1f2622
Danyx <marcop.morocho17@gmail.com>
2026-09-26 22:26:57 +0000

    feat(system_diagnostic):Hize el bloque 3

--- system_diagnostics.ino
+++ system_diagnostics.ino
@@ -11,19 +11,14 @@ void runSystemPOST() {
     Serial.println("   ESP32: POWER-ON SELF TEST (POST)       ");
     Serial.println("==========================================");
 
-    // TODO 4.1: Mostrar la cabecera del sistema invocando showBootHeader()
-    /* ESCRIBE TU CÓDIGO AQUÍ */
+    showBootHeader();
 
-    // TODO 4.2: Probar secuencialmente los 4 subsistemas de la estación usando logBoot(nombre, estado):
-    // 1. "ESP32 240MHz"  -> true
-    // 2. "I2C @ 400kHz"  -> true
-    // 3. "OLED 0x3C"     -> true
-    // 4. "Bateria 8.4V"  -> true
-    // Pregunta Guía: ¿Qué ventaja tiene usar una función logBoot() frente a repetir 15 líneas de código por módulo?
-    /* ESCRIBE TU CÓDIGO AQUÍ */
+    logBoot("ESP32 240MHz", true);
+    logBoot("I2C @ 400kHz", true);
+    logBoot("OLED 0x3C", true);
+    logBoot("Bateria 8.4V", true);
 
     delay(500);
 
-    // TODO 4.3: Concluir la rutina mostrando el mensaje de sistema operativo invocando showSystemReady()
-    /* ESCRIBE TU CÓDIGO AQUÍ */
+    showSystemReady();
 }
